4197be92e9550a1c51880ae2d786844a4e84f2ca
ntminhdn <ntminhdn@gmail.com>
2021-06-12 09:39:38 +0700

    fix config ios (#5)
    Co-authored-by: Nguyen Thanh Minh <minhnt3@nal.vn>

--- lib/app_setup_and_run.dart
+++ lib/app_setup_and_run.dart
@@ -13,6 +13,7 @@ import 'di/di.dart';
 import 'presentation/helper/deeplink/deep_link.dart';
 import 'presentation/helper/firebase_messaging/firebase_messaging.dart';
 import 'presentation/ui/app.dart';
+import 'utils/stream/dispose_bag.dart';
 import 'utils/stream/stream_logger.dart';
 
 class App {
@@ -30,11 +31,8 @@ class App {
     /// config Equatable
     EquatableConfig.stringify = true;
 
-    /// config http request logging
-    HttpRequestLogConfig.enableLogInterceptor = kDebugMode;
-
-    /// config stream logging
-    StreamLoggerConfig.enableStreamLogger = kDebugMode;
+    /// config logging
+    _configLogging();
 
     await Future.wait([
       SystemChrome.setEnabledSystemUIOverlays(
@@ -57,5 +55,16 @@ class App {
   static _reportError(Object error, StackTrace stacktrace) {
     print('uncaught error: $error');
     // report error by sentry or firebase crashlytic
+  }
+
+  static _configLogging() {
+    /// config http request logging
+    HttpRequestLogConfig.enableLogInterceptor = kDebugMode;
+
+    /// config stream logging
+    StreamLoggerConfig.enableStreamLogger = kDebugMode;
+
+    /// config DisposeBag logging
+    DisposeBagConfig.enableLogging = kDebugMode;
   }
 }
